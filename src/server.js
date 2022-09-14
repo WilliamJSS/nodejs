68d6205ec60e3d2581095c7c9e25ceea48189a3e
WilliamJSS <williamsena088@gmail.com>
2022-09-13 21:48:12 -0300

    fix: chamada do cors

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
 
-app.use(cors())
-
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
